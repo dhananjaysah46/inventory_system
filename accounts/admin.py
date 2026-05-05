86eb1b3054492279c0c04fee73a911ff29d1223d
Dhananjay Sah <dhanjusah123@gmail.com>
2026-05-05 17:38:56 +0545

    refine product page ui, along with admin role manager implementation

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'is_active')
+    list_filter = ('role')
+    search_fields = ('username', 'email')
